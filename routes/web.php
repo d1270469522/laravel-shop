dd4b04dfb6c6d356515822ef58fa9951c5712ba6
董鹏飞 <1270469522@qq.com>
2020-05-22 03:29:21 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 在之前的路由后面配上中间件
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
+// 在之前的路由里加上一个 verify 参数
+Auth::routes(['verify' => true]);
