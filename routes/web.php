170205e1215ec21e8349038a2a80c0eadf142812
董鹏飞 <1270469522@qq.com>
2020-06-03 11:03:11 +0000

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 // 在之前的路由后面配上中间件
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
